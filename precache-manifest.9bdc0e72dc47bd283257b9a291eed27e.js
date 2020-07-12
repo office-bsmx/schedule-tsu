d0806fa687f2cac014b1e97422e86400aae6f8d9
Max <office@bsmx.ru>
2020-07-12 17:09:02 +0300

    Updates

---
+++ precache-manifest.9bdc0e72dc47bd283257b9a291eed27e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d58c023bd6999233674dd11d3416ec77",
+    "url": "/schedule-tsu/index.html"
+  },
+  {
+    "revision": "5ec3ac19a23340124d0a",
+    "url": "/schedule-tsu/static/css/2.a6674286.chunk.css"
+  },
+  {
+    "revision": "14f31c13bfb402b7cd7b",
+    "url": "/schedule-tsu/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5ec3ac19a23340124d0a",
+    "url": "/schedule-tsu/static/js/2.e8ecc446.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/schedule-tsu/static/js/2.e8ecc446.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14f31c13bfb402b7cd7b",
+    "url": "/schedule-tsu/static/js/main.d45b9fe7.chunk.js"
+  },
+  {
+    "revision": "2acb1d443962a0725b0f",
+    "url": "/schedule-tsu/static/js/runtime-main.48647c6c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/schedule-tsu/static/media/logo.5d5d9eef.svg"
+  }
+]);
